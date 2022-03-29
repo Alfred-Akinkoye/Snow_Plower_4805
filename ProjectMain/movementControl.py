b8694c2cefec3284b78a1796c8143f37430239dc
Ester <chowed85@gmail.com>
2022-03-29 13:43:40 -0400

    Copied in changes from rotation control

--- ProjectMain/movementControl.py
+++ ProjectMain/movementControl.py
@@ -1,4 +1,5 @@
 import time
+from math import pi
 
 class MovementControl():
     def __init__(self, plow, api):
@@ -47,6 +48,54 @@ class MovementControl():
     def forwardRotation(self, degrees):
         # Rotate while moving forward (A set distance) in order to keep the snow in the plow
         pass
+
+    def rotate_Amount(self, radians):
+        # Rotate a set number of degrees (Either ***by checking orientation*** or by time? or by wheel encoding)
+        current = self.getObjectOrientaion()
+        target = current + radians
+        print(target)
+        
+        #if new angle greater than pi, readjust
+        if (target > pi):
+            target = -(pi - (target-pi))
+        elif (target < -pi):
+            target = pi+(target+pi)
+        
+        print(target)
+        #rotate based on direction given
+        if (radians >0):
+            self.rotate(1)
+        else:
+            self.rotate(-1)
+
+        #rotate until target equals point
+        difference = abs(target - self.getObjectOrientaion())
+        while(difference> 0.025):
+            #print(difference)
+            difference = abs(target - self.getObjectOrientaion())
+        self.stop()
+
+    def rotate_Facing(self,orientation,direction):
+        """
+        Rotate plower into specific cardinal direction
+        Args:
+        orientation = enter N,E,S, or W for direction to turn to
+        direction = enter True for CW rotation, false for CCW
+        """
+        od = {"N":0, "E": -pi/2, "S":pi, "W":pi/2}
+        target = od[orientation]
+
+        if (direction >0):
+            self.rotate(-1)
+        else:
+            self.rotate(1)
+
+        difference = abs(target - self.getObjectOrientaion())
+        while(difference> 0.025):
+            #print(difference)
+            difference = abs(target - self.getObjectOrientaion())
+        self.stop()
+
     
 class Wheel():
     def __init__(self, api, handle):
