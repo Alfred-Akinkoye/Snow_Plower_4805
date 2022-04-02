1c18beaff2d4670f2167044f3c389b3a62c92932
dejisayomi <dejisayomi@cmail.carleton.ca>
2022-04-01 22:38:46 -0400

    docs: API documentation
    Updated the api.py documentation with docstrings.

--- ProjectMain/api.py
+++ ProjectMain/api.py
@@ -16,11 +16,18 @@ except:
 
 import math
 
+
 class API:
+    """
+    API class contains methods which allows our Python code to connect to 
+    CoppeliaSim robot
+    """
     def __init__(self):
         self.clientID = None
 
     def connect(self):
+        """Connects to CoppeliaSim"""
+
         print ('Connecting to remote API server...')
         sim.simxFinish(-1) # just in case, close all opened connections
         self.clientID = sim.simxStart('127.0.0.1',19999,True,True,5000,5) # Connect to CoppeliaSim
@@ -33,15 +40,22 @@ class API:
         return False
 
     def disconnect(self):
+        """Disconnects from CoppeliaSim"""
+
         # Before closing the connection to CoppeliaSim, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
         sim.simxGetPingTime(self.clientID)
         # Now close the connection to CoppeliaSim:
         sim.simxFinish(self.clientID)
     
     def stopSimulation(self):
+        """Stops the simulation"""
         sim.simxStopSimulation(self.clientID, sim.simx_opmode_oneshot)
 
     def disconnected(self):
+        """
+        Returns 'True' sucessfully disconected from CoppeliaSim.
+        Otherwise, method returns 'False'.
+        """
         connectionID = sim.simxGetConnectionId(self.clientID)
         if (connectionID == -1):
             return True
@@ -49,41 +63,43 @@ class API:
 
     # ADD FUNCTIONS TO CALL API FUNCTIONS HERE (AS DESIRED)
 
-    # Messages (Logging)
     def sendMessage(self, message):
+        """Messages (Logging)"""
         sim.simxAddStatusbarMessage(self.clientID, message, sim.simx_opmode_oneshot)
 
-    # Get Object by Handle
     def getObject(self, name):
+        """Get Object by Handle"""
         return sim.simxGetObjectHandle(self.clientID, name, sim.simx_opmode_blocking)[1]
 
-    # Return the position of a joint (This is just a checker, or a limiter)
     def getJointPosition(self, joint):
+        """Return the position of a joint (This is just a checker, or a limiter)."""
         return sim.simxGetJointPosition(self.clientID, joint, sim.simx_opmode_blocking)[1]
 
-    # Get vision sensor Data
     def readVisionSensor(self, name):
+        """Get vision sensor data from robot in CoppeliaSim."""
         return sim.simxReadVisionSensor(self.clientID, name, sim.simx_opmode_blocking)
 
-        #Get proximity sensor data
     def readProximitySensor(self, name):
+        """Get proximity sensor data from robot in CoppeliaSim."""
         return sim.simxReadProximitySensor(self.clientID, name, sim.simx_opmode_blocking)
 
 
-    # Positional Tracking
     def getObjectPosition(self, handle):
+        """Positional Tracking. Obtains position of robot."""
         return sim.simxGetObjectPosition(self.clientID, handle, -1, sim.simx_opmode_blocking)
 
     def getObjectOrientation(self, handle):
+        """Determines the position of the robot in CoppeliaSim."""
         return sim.simxGetObjectOrientation(self.clientID, handle, -1, sim.simx_opmode_blocking) 
 
-    # Movement
     def setJointVelocity(self, joint, velocity):
+        """Movement: Sets the velocity of the robot's wheels."""
         wheelCircumfrence = math.pi * 0.12705 # Robot Wheels are 0.12705m in diameter
         linearVelocity = velocity # Velocities in python program should be in m/s (Linear)
         angularVelocity = linearVelocity * ((2 * math.pi) / wheelCircumfrence) # Velocities passed to CoppeliaSim should be in rads/s
         return sim.simxSetJointTargetVelocity(self.clientID, joint, angularVelocity, sim.simx_opmode_oneshot)
 
     def setJointPosition(self, joint, position):
+        """Movement: Sets the position of the robot's wheels"""
         return sim.simxSetJointTargetPosition(self.clientID, joint, position, sim.simx_opmode_oneshot)
 
